169836be88dff84d263d66e3a2c080efe5c2d572
al060879 <89102157+Ary78956@users.noreply.github.com>
2021-11-05 14:23:00 -0600

    Add files via upload

---
+++ calculadora.java
@@ -0,0 +1,22 @@
+
+package clases;
+
+public class calculadora {
+    public int sumar(int a, int b ){
+        int suma = a + b;
+        return suma;
+    }
+    public int restar(int a, int b){
+        int resta = a - b;
+        return resta;
+    }
+    public int multiplicar(int a, int b){
+        int multiplicar = a * b;
+        return multiplicar;
+    }
+    public int dividir(int a, int b){
+        int dividir = a / b;
+        return dividir;
+    }
+
+}
